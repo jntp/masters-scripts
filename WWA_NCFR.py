6fc31136a95ca6df65ad288737556d8d91e00a5f
Justin Tang <justin.jntprules@gmail.com>
2021-11-15 16:31:33 -0800

    write/modified code in the ff section to properly obtain datetimes and ff time threshold

--- WWA_NCFR.py
+++ WWA_NCFR.py
@@ -1,7 +1,14 @@
 '''
 Script for reading WWAs and NCFR catalog csv files. Checks to see if time matches for flash flood events
 '''
-import datetime as dt 
+import datetime as dt
+from statistics import mean 
+
+# def isFFwithinNCFR(FF_start_time, FF_end_time, NCFR_start_time, NCFR_end_time):
+# First figure out an appropriate threshold... you will need to justify this in your thesis!
+# also the NCFR catalog is weird... if the end time is past 00:00 it will still be considered the same date
+# also review FF criteria... because FF be issued well after NCFR has passed
+# review what type of urban flooding you're look at in yo thesis
 
 def main():
   wwa_fp = './data/WWA_All_1995_2020.csv'
@@ -19,18 +26,14 @@ def main():
   ncfr_file.flush()
   ncfr_file.close() 
 
-  # Lists
-  ff_start_years = []
-  ff_start_months = []
-  ff_start_days = []
-  ff_start_hours = []
-  ff_start_mins = []
-  ff_end_years = []
-  ff_end_months = []
-  ff_end_days = []
-  ff_end_hours = []
-  ff_end_mins = []
- 
+  # Lists to obtain from the WWA file
+  ff_start_datetimes = [] 
+  ff_end_datetimes = []
+  ff_time_lengths = [] 
+
+  # Initialize stored variables
+  ff_time_sum = dt.timedelta() # start with a duration of 0:00:00; used as sum for mean calculation
+
   for line in wwa_lines:
     components = line.split(",")
     # 1 = year, 2 = month, 3 = day, 4 = hour, 5 = min for start times
@@ -38,24 +41,51 @@ def main():
 
     if 'FF' in components:
       try:
-        ff_start_years.append(int(components[1]))
-        ff_start_months.append(int(components[2]))
-        ff_start_days.append(int(components[3]))
-        ff_start_hours.append(int(components[4]))
-        ff_start_mins.append(int(components[5]))
-        ff_end_years.append(int(components[6]))
-        ff_end_months.append(int(components[7]))
-        ff_end_days.append(int(components[8]))
-        ff_end_hours.append(int(components[9]))
-        ff_end_mins.append(int(components[10]))
+        start_datetime = dt.datetime(int(components[1]), int(components[2]), int(components[3]), int(components[4]), \
+            int(components[5]))
+        end_datetime = dt.datetime(int(components[6]), int(components[7]), int(components[8]), int(components[9]), \
+            int(components[10]))
       except:
         continue
-    
-  print(ff_start_years) # test these 
+      else:
+        ff_start_datetimes.append(start_datetime) 
+        ff_end_datetimes.append(end_datetime)
 
-  # print(dt.datetime(2020, 2, 16, 5, 30)) 
+        ff_length = end_datetime - start_datetime
+        ff_time_lengths.append(ff_length)
+        ff_time_sum += ff_length 
+
+  # Find the mean of the flash flood warning time lengths to get the "threshold" 
+  ff_treshold = ff_time_sum / len(ff_time_lengths)
+    
+  # Lists to obtain from the NCFR file
+  ncfr_years = []
+  ncfr_months = []
+  ncfr_days = []
+  ncfr_start_hours = []
+  ncfr_end_hours = []
+
+  # Now time for NCFR bitches!!! 
+  for line in ncfr_lines:
+    components_ncfr = line.split(",")
+    # 1 = year, 2 = month, 3 = day, 4 = start_hour, 5 = end_hour
+
+    try: 
+      ncfr_years.append(int(components_ncfr[1]))
+      ncfr_months.append(int(components_ncfr[2]))
+      ncfr_days.append(int(components_ncfr[3]))
+      ncfr_start_hours.append(int(components_ncfr[4]))
+      ncfr_end_hours.append(int(components_ncfr[5]))
+    except:
+      continue 
+
+  test1 = dt.datetime(2020, 2, 16, 5, 30)
+  test2 = dt.datetime(2020, 2, 16, 8, 40)
+
+  
+   
 
 if __name__ == '__main__':
   main()
 
-# Next step... for loop for NCFR_lines, then for loop for matching or something? 
+# Next step... Write isFFwithinNCFR function 
