3d364d6d292cfd9f3ff8eef800a51fcf85301363
Justin Tang <justin.jntprules@gmail.com>
2022-06-01 02:00:55 -0700

    wrote most of flood_count code... need to finish check_threshold function

--- flood_count.py
+++ flood_count.py
@@ -1,6 +1,66 @@
 import pandas as pd
+import numpy as np
+import datetime as dt
+
+def convert_str_to_dt(datetime_str):
+  # Create slice objects for parsing string
+  year_slc = slice(0, 4)
+  month_slc = slice(5, 7)
+  day_slc = slice(8, 10)
+  hour_slc = slice(11, 13)
+  min_slc = slice(14, 16)
+
+  # Parse string accordingly
+  year = datetime_str[year_slc]
+  month = datetime_str[month_slc]
+  day = datetime_str[day_slc]
+  hour = datetime_str[hour_slc]
+  minute = datetime_str[min_slc]
+
+  # Create datetime object
+  datetime = dt.datetime(int(year), int(month), int(day), int(hour), int(minute))
+
+  return datetime
+
+def load_streamflow_data(file_path):
+  entries = pd.read_csv(file_path)
+  datetimes_str = entries.loc[:, "datetime"]
+  discharges_cfs_str = entries.loc[:, "discharge_cfs"]
+
+  # Create new lists that will hold converted datetime and discharge data
+  datetimes = []
+  discharges_cfs = []
+
+  # Loop through entries and convert datetime and discharge data
+  for i, datetime in enumerate(datetimes_str): 
+    datetimes.append(convert_str_to_dt(datetimes_str[i]))
+    discharges_cfs.append(int(discharges_cfs_str[i]))
+
+  return datetimes, discharges_cfs
+
+def check_threshold(stream_datetimes, stream_Qs, start_datetime, end_datetime, discharge_threshold):
+  stream_datetimes = np.array(stream_datetimes)
+  stream_Qs = np.array(stream_Qs)
+
+  time_inds = np.where((stream_datetimes >= start_datetime) & (stream_datetimes <= end_datetime))[0]
+
+  # Finish writing this code
+
 
 def main():
+  ## Load streamflow data
+  # Get the file paths
+  sepulveda_fp = "./data/sepulveda_15min_discharge_2002_2020.csv"
+  whittier_fp = "./data/whittier_15min_discharge_1995_2020.csv"
+  santa_ana_fp = "./data/santa_ana_15min_discharge_1995_2020.csv"
+  san_diego_fp = "./data/san_diego_15min_discharge_1995_2020.csv"
+
+  # Load streamflow data
+  sepulveda_dts, sepulveda_Qs = load_streamflow_data(sepulveda_fp)
+  whittier_dts, whittier_Qs = load_streamflow_data(whittier_fp)
+  santa_ana_dts, santa_ana_Qs = load_streamflow_data(santa_ana_fp)
+  san_diego_dts, san_diego_Qs = load_streamflow_data(san_diego_fp) 
+
   ## Find flash flood warnings from WWA catalog
   # Load from csv file
   wwa_fp = "./data/WWA_All_1995_2020.csv"
@@ -12,13 +72,63 @@ def main():
   beg_hours = wwa_entries.loc[:, "BegHur"]
   beg_mins = wwa_entries.loc[:, "BegMin"]
   end_years = wwa_entries.loc[:, "EndYear"]
-  end_months = wwa_entries.loc[:, "EndDay"]
-  end_days = wwa_entries.loc[:, "EndHur"]
+  end_months = wwa_entries.loc[:, "EndMon"]
+  end_days = wwa_entries.loc[:, "EndDay"]
+  end_hours = wwa_entries.loc[:, "EndHur"]
   end_mins = wwa_entries.loc[:, "EndMin"]
   phenoms = wwa_entries.loc[:, "PHENOM"]
   statuses = wwa_entries.loc[:, "STATUS"] 
 
-  # Run through catalog, look for new or cont ffws 
+  # Get indices where entry meets criteria of being a new or continued FFW
+  ffw_inds = phenoms[phenoms == "FF"].index.tolist() 
+  new_inds = statuses[statuses == "NEW"].index.tolist()
+  con_inds = statuses[statuses == "CON"].index.tolist()
+
+  # Convert lists to numpy array for array operations
+  ffw_inds = np.array(ffw_inds)
+  new_inds = np.array(new_inds)
+  con_inds = np.array(con_inds) 
+
+  # Append the new and continued indices and sort them numerically
+  new_con_inds = np.append(new_inds, con_inds)
+  new_con_inds = np.sort(new_con_inds)
+
+  # Create a list to store all the indices that meet the criteria
+  right_inds = []
+
+  # Find the indices where new_con_inds and ffw_inds match
+  for new_con_ind in new_con_inds:
+    if np.any(ffw_inds == new_con_ind):
+      right_inds.append(new_con_ind)
+
+  ## Check if the streamflow of each watershed exceeds threshold during the each FFW event
+  # Get entries from WWA catalog that meet the criteria
+  wfos_ff = wfos[right_inds]
+  beg_years_ff = beg_years[right_inds]
+  beg_months_ff = beg_months[right_inds]
+  beg_days_ff = beg_days[right_inds]
+  beg_hours_ff = beg_hours[right_inds]
+  beg_mins_ff = beg_mins[right_inds]
+  end_years_ff = end_years[right_inds]
+  end_months_ff = end_months[right_inds]
+  end_days_ff = end_days[right_inds]
+  end_hours_ff = end_hours[right_inds]
+  end_mins_ff = end_mins[right_inds]
+
+  # Create another list to store all the indices that meet both FFW criteria and streamflow threshold
+  flood_inds = []
+
+  # Loop through each FFW entry; check if the streamflow during FFW time (and up to 3 hours later)
+  for i in right_inds:
+    start_dt = dt.datetime(int(beg_years_ff[i]), int(beg_months_ff[i]), int(beg_days_ff[i]), int(beg_hours_ff[i]), \
+        int(beg_mins_ff[i]))
+    end_dt = dt.datetime(int(end_years_ff[i]), int(end_months_ff[i]), int(end_days_ff[i]), int(end_hours_ff[i]), \
+        int(end_mins_ff[i]))
+
+    check_threshold(san_diego_dts, san_diego_Qs, start_dt, end_dt, 1000)
+
+# Note! There are multiple FFWs for the same times... please address this
+    
 
 if __name__ == '__main__':
   main()
